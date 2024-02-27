c8e855973e12d633db84fcf7ffcef219827843fd
giovanayokota <gnyokota@gmail.com>
2024-02-27 20:16:00 +0100

    :sparkles: inital commit

---
+++ src/main/kotlin/com/reactive/programming/ProgrammingApplication.kt
@@ -0,0 +1,11 @@
+package com.reactive.programming
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProgrammingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProgrammingApplication>(*args)
+}
