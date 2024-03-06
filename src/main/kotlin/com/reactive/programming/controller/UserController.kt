235aee9f58ef34272725dc6d17ebbe5961b8443a
giovanayokota <gnyokota@gmail.com>
2024-03-06 16:39:27 +0100

    :sparkles: add logger

--- src/main/kotlin/com/reactive/programming/controller/UserController.kt
+++ src/main/kotlin/com/reactive/programming/controller/UserController.kt
@@ -17,7 +17,8 @@ import reactor.kotlin.core.publisher.switchIfEmpty
 @RequestMapping("/api/v1")
 class UserController(val userRepository: UserRepository, val userWebClient: WebClient) {
 
-    private val logger =  LoggerFactory.getLogger(UserController::class.java)
+    private val logger = LoggerFactory.getLogger(UserController::class.java)
+
     @GetMapping("/users")
     fun getAllUser(): ResponseEntity<Flux<UserInput>> {
         return ResponseEntity.ok(userRepository.findAll())
@@ -29,37 +30,37 @@ class UserController(val userRepository: UserRepository, val userWebClient: WebC
         return ResponseEntity.ok(foundUser.map {
             it.toUserInputResponse()
         }.switchIfEmpty { Mono.error(UserNotFound("User $id could not be found!", id.toLong())) }
-            .doFirst{logger.info("Retrieving user with id:$id")}
-            .doOnError{ exc -> logger.error("Something went wrong while retrieveing user: $id, ${exc.message}")}
+            .doFirst { logger.info("Retrieving user with id:$id") }
+            .doOnError { exc -> logger.error("Something went wrong while retrieveing user: $id, ${exc.message}") }
             .doFinally { signalType -> logger.info("Finalized retrieving user: $signalType") })
     }
 
     @PostMapping("/user")
     fun saveUser(@RequestBody userInput: UserInput): ResponseEntity<Mono<UserInputResponse>> {
-        val createdUser = userRepository.insert(userInput).map{
+        val createdUser = userRepository.insert(userInput).map {
             it.toUserInputResponse()
-        }.onErrorMap {  exc-> UserNotCreated("User could not be created: ${exc.message}!")  }
+        }.onErrorMap { exc -> UserNotCreated("User could not be created: ${exc.message}!") }
         return ResponseEntity.ok(createdUser)
     }
 
     @PostMapping("/users/ext")
     fun saveExternalUsers(): ResponseEntity<Flux<UserInputResponse>> {
-        val externalUsers = userWebClient.get()
+        val externalUsers  = userWebClient.get()
             .uri("/users")
             .retrieve()
             .bodyToFlux(UserInput::class.java)
 
-        val createdUser = userRepository.insert(externalUsers).map{
+        val createdUser = userRepository.insert(externalUsers).map {
             it.toUserInputResponse()
-        }.doOnError {  exc-> UserNotCreated("User could not be created: ${exc.message}!")  }
+        }.doOnError { exc -> UserNotCreated("User could not be created: ${exc.message}!") }
         return ResponseEntity.ok(createdUser)
     }
 
     @DeleteMapping("/users/{id}")
-    fun deleteUserById(@PathVariable id:String): ResponseEntity<Any>{
-        return ResponseEntity.ok(userRepository.deleteById(id).doOnError {
-            exc -> logger.error("User $id could not be deleted: ${exc.message}")
-        }.doFinally{ signalType->
+    fun deleteUserById(@PathVariable id: String): ResponseEntity<Any> {
+        return ResponseEntity.ok(userRepository.deleteById(id).doOnError { exc ->
+            logger.error("User $id could not be deleted: ${exc.message}")
+        }.doFinally { signalType ->
             logger.info("User $id successfully deleted: $signalType")
         })
     }
